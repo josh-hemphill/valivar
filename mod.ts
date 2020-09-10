1a94527a3dea6cfadf28b236a36e676da8eb836c
Josh <46608115+josh-hemphill@users.noreply.github.com>
2020-09-09 23:45:26 -0400

    Update mod.ts

--- mod.ts
+++ mod.ts
@@ -1,6 +1,6 @@
 // @deno-types="./dist/dot.d.ts"
 import dot from './dist/dot.js';
-// @deno-types="./dist/validate.d.ts"
+// @deno-types="./dist/valivar.d.ts"
 import validate from './dist/valivar.js';
 // @deno-types="./dist/typecast.d.ts"
 import typecast from './dist/typecast.js';
