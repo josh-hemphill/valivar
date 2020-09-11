91d11ab2639a05506d421af0c77a31bcc1f436bf
jhemphill <jhemphill@tecnicocorp.com>
2020-09-11 05:40:21 -0400

    fix: deno imports typo

--- mod.ts
+++ mod.ts
@@ -1,13 +1,7 @@
 // @deno-types="./dist/dot.d.ts"
-<<<<<<< HEAD
 import { dot } from './dist/dot.js';
 // @deno-types="./dist/valivar.d.ts"
 import { Schema as valivar } from './dist/valivar.js';
-=======
-import dot from './dist/dot.js';
-// @deno-types="./dist/valivar.d.ts"
-import validate from './dist/valivar.js';
->>>>>>> 1a94527a3dea6cfadf28b236a36e676da8eb836c
 // @deno-types="./dist/typecast.d.ts"
 import { typecast } from './dist/typecast.js';
 
