570a2f105fd29b51749e3fcfc207a8983632fac4
jhemphill <jhemphill@tecnicocorp.com>
2020-09-14 21:58:38 -0400

    fix(release): crypto config

--- dist/valivar.cjs.js
+++ dist/valivar.cjs.js
@@ -383,6 +383,12 @@ class ValidationError extends Error {
 ***************************************************************************************/
 const toString = Object.prototype.toString;
 const funToString = Function.prototype.toString;
+const localGlobal = {
+  Buffer: typeof Buffer !== 'undefined' ? Buffer : ArrayBuffer,
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  // @ts-ignore
+  globalThis: typeof globalThis !== 'undefined' ? globalThis : typeof window !== 'undefined' ? window : {}
+};
 /**
  * Return the type of `val` as a string.
  *
@@ -414,12 +420,6 @@ function getType(val) {
 
   if (val === null) return 'null';
   if (val === undefined) return 'undefined';
-
-  if (this) {
-    if (!this?.Buffer) this.Buffer = ArrayBuffer;
-    if (!this.globalThis) this.globalThis = {};
-  }
-
   if (typeof val === 'number' && isNaN(val)) return 'nan';
   if (typeof val === 'object' && isElement(val)) return 'element';
   if (typeof val === 'object' && isNode(val)) return 'node';
@@ -440,24 +440,20 @@ function getType(val) {
     return !!( // Does not support Safari 5-7 (missing Object.prototype.constructor)
     // Accepted as Safari 5-7 (Mobile & Desktop) is at < 0.17% usage
     // https://caniuse.com/usage-table
-    obj instanceof Buffer);
+    obj instanceof localGlobal.Buffer);
   } // HTML Type Checking from https://stackoverflow.com/questions/384286/how-do-you-check-if-a-javascript-object-is-a-dom-object
   //Returns true if it is a DOM node
 
 
   function isNode(o) {
     const globalKey = 'Node';
-    return isGlobal(globalKey) ? o instanceof globalThis[globalKey] : o && isWholeObject(o) && typeof o.nodeType === 'number' && typeof o.nodeName === 'string';
+    return Object.prototype.hasOwnProperty.call(localGlobal.globalThis, globalKey) ? o instanceof localGlobal.globalThis[globalKey] : o && isWholeObject(o) && typeof o.nodeType === 'number' && typeof o.nodeName === 'string';
   } //Returns true if it is a DOM element
 
 
   function isElement(o) {
     const globalKey = 'HTMLElement';
-    return isGlobal(globalKey) ? o instanceof globalThis[globalKey] : o && isWholeObject(o) && o.nodeType === 1 && typeof o.nodeName === 'string';
-  }
-
-  function isGlobal(name) {
-    return Object.prototype.hasOwnProperty.call(globalThis, name);
+    return Object.prototype.hasOwnProperty.call(localGlobal.globalThis, globalKey) ? o instanceof localGlobal.globalThis[globalKey] : o && isWholeObject(o) && o.nodeType === 1 && typeof o.nodeName === 'string';
   }
 }
 
