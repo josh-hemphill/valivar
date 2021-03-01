e1131c4d557df1cb58df996069e2c0e34703c80c
josh-hemphill <jhemphill@tecnicocorp.com>
2021-03-01 12:46:12 -0500

    ci: :heavy_minus_sign: remove old release code/dep

--- release/config.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-module.exports = {/* 
-	defaultShell:'C:\\Program Files\\Git\\bin\\bash.exe',
-	githubApiUrl: 'https://api.github.com',
-	SSH_HostNames: ['github','github.com'],
-	sshConfig: '~/.ssh/config',
-	deployStorage: 'valivarDeploy.enc', */
-};
